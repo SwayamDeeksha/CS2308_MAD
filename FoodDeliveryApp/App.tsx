61674e22bd2743b201e8fc3f92c6852bf6962d7e
Swayam Deeksha <swadeeku.007@gmail.com>
2026-02-24 22:19:36 +0530

    Remove unused useMemo import from App.tsx

--- FoodDeliveryApp/App.tsx
+++ FoodDeliveryApp/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { SafeAreaView, View, Text, ScrollView, StyleSheet, StatusBar, Dimensions } from 'react-native';
 import menuData from './menu.json';
 
